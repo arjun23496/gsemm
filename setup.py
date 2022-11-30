eefa57e356daf622d5e03732834288ba9b93e963
arjun23496 <arjun.k018@gmail.com>
2022-11-30 14:40:17 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='episodic_memory',
+      version='1.0',
+      description='Python Modules for Episodic Memory',
+      author='Arjun Karuvally',
+      author_email='akaruvally@umass.edu'
+      )
